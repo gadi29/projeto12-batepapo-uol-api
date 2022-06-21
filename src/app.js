64493192096be3275dad45086babb2f3af475e84
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-06-21 15:27:02 -0300

    create app.js and set server port

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express, { json } from "express";
+import cors from "cors";
+
+const app = express();
+app.use(json());
+app.use(cors());
+const port = 5000;
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+})
