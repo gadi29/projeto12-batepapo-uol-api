7d7155d09c363f18e0876eaf16a188172c68df71
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-06-22 10:51:17 -0300

    implement dotenv

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 import express, { json } from "express";
 import cors from "cors";
+import dotenv from 'dotenv';
+import { MongoClient } from "mongodb";
 
 const app = express();
 app.use(json());
 app.use(cors());
+dotenv.config();
 const port = 5000;
 
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
 
+mongoClient.connect().then(() => {
+	db = mongoClient.db("batepapo_uol");
+});
 
 
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-})
+});
