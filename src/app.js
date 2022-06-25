1c6b92b03ec164281d3df04b107d04c4d2abd093
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-06-25 11:37:36 -0300

    create POST /participants

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import cors from "cors";
 import dotenv from 'dotenv';
 import { MongoClient } from "mongodb";
+import joi from 'joi';
 
 const app = express();
 app.use(json());
@@ -16,7 +17,32 @@ mongoClient.connect().then(() => {
 	db = mongoClient.db("batepapo_uol");
 });
 
+const participantSchema = joi.object({
+  name: joi.string().required()
+})
 
+app.post('/participants', async (req, res) => {
+  const newParticipant = req.body;
+
+  const validate = participantSchema.validate(newParticipant);
+  if (validate.error) {
+    return res.sendStatus(422);
+  }
+
+  const participant = await db.collection('participants').findOne({ name: newParticipant.name });
+  if (participant) {
+    console.log(participant)
+    return res.sendStatus(409);
+  }
+
+  try {
+    await db.collection('participants').insertOne({ ...newParticipant, lastStatus: Date.now() });
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500);
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
