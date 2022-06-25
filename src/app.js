a8c6f781cda83be8c4093cbe24ca6b37b793e533
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-06-25 12:49:08 -0300

    create POST /messages

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import express, { json } from "express";
-import cors from "cors";
+import express, { json } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 import joi from 'joi';
 
 const app = express();
@@ -19,7 +19,13 @@ mongoClient.connect().then(() => {
 
 const participantSchema = joi.object({
   name: joi.string().required()
-})
+});
+
+const messageSchema = joi.object({
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().required()
+});
 
 app.get('/participants', async (req, res) => {
   try {
@@ -29,7 +35,7 @@ app.get('/participants', async (req, res) => {
     console.error(error);
     res.sendStatus(500);
   }
-})
+});
 
 app.post('/participants', async (req, res) => {
   const newParticipant = req.body;
@@ -41,7 +47,6 @@ app.post('/participants', async (req, res) => {
 
   const participant = await db.collection('participants').findOne({ name: newParticipant.name });
   if (participant) {
-    console.log(participant)
     return res.sendStatus(409);
   }
 
@@ -52,7 +57,38 @@ app.post('/participants', async (req, res) => {
     console.error(error)
     res.sendStatus(500);
   }
-})
+});
+
+app.post('/messages', async (req, res) => {
+  const newMessage = req.body;
+
+  const validate = messageSchema.validate(newMessage, { abortEarly: false });
+  if (validate.error) {
+    return res.sendStatus(422);
+  }
+
+  if (newMessage.type !== "message" && newMessage.type !== "private_message") {
+    return res.sendStatus(422);
+  }
+
+  const userName = req.headers.user;
+  const message = { ...newMessage, from: userName };
+  
+  const user = await db.collection('participants').findOne({ name: userName });
+  if (!user) {
+    return res.sendStatus(422);
+  }
+
+  try {
+    const now = "10:00";
+
+    await db.collection('messages').insertOne({ ...message, time: now })
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
